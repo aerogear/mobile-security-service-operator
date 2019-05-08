48383f51e8a5208e58df3ec9b5bb6bb89dae7ff3
Enda Phelan <ephelan@redhat.com>
2019-05-08 12:34:53 +0100

    test(app): added test for create() (AEROGEAR-8951)

--- pkg/controller/mobilesecurityservice/controller_test.go
+++ pkg/controller/mobilesecurityservice/controller_test.go
@@ -1,10 +1,15 @@
 package mobilesecurityservice
 
 import (
+	errs "errors"
 	"reflect"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
 	"github.com/aerogear/mobile-security-service-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	mobilesecurityservicev1alpha1 "github.com/aerogear/mobile-security-service-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	errors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -12,91 +17,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
-
-// func TestReconcileMobileSecurityService_Reconcile(t *testing.T) {
-
-// 	var (
-// 		name                                = "mobile-security-service-operator"
-// 		namespace                           = "mobile-security-service-app"
-// 		size                          int32 = 1
-// 		image                               = "aerogear/mobile-security-service:master"
-// 		databaseName                        = "mobile_security_service"
-// 		databaseHost                        = "mobile-security-service-db"
-// 		port                          int32 = 3000
-// 		logLevel                            = "info"
-// 		logFormat                           = "text"
-// 		accessControlAllowOrigin            = "*"
-// 		accessControlAllowCredentials       = "false"
-// 		memoryLimit                         = "512Mi"
-// 		memoryRequest                       = "512Mi"
-// 		clusterHost                         = "192.168.0.1"
-// 		protocol                            = "http"
-// 		hostSuffix                          = ".nip.io"
-// 		configMapName                       = "mss-config"
-// 	)
-
-// 	instance := &mssv1alpha1.MobileSecurityService{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      name,
-// 			Namespace: namespace,
-// 		},
-// 		Spec: v1alpha1.MobileSecurityServiceSpec{
-// 			Size:                          size,
-// 			Image:                         image,
-// 			DatabaseHost:                  databaseHost,
-// 			DatabaseName:                  databaseName,
-// 			Port:                          port,
-// 			LogLevel:                      logLevel,
-// 			LogFormat:                     logFormat,
-// 			AccessControlAllowOrigin:      accessControlAllowOrigin,
-// 			AccessControlAllowCredentials: accessControlAllowCredentials,
-// 			MemoryLimit:                   memoryLimit,
-// 			MemoryRequest:                 memoryRequest,
-// 			ClusterHost:                   clusterHost,
-// 			Protocol:                      protocol,
-// 			HostSufix:                     hostSuffix,
-// 			ConfigMapName:                 configMapName,
-// 		},
-// 	}
-
-// 	objs := []runtime.Object{
-// 		instance,
-// 	}
-
-// 	s := scheme.Scheme
-// 	s.AddKnownTypes(v1alpha1.SchemeGroupVersion, instance)
-// 	cl := fake.NewFakeClient(objs...)
-
-// 	r := &ReconcileMobileSecurityService{cl, s}
-
-// 	req := reconcile.Request{
-// 		NamespacedName: types.NamespacedName{
-// 			Name:      name,
-// 			Namespace: namespace,
-// 		},
-// 	}
-
-// 	res, err := r.Reconcile(req)
-// 	if err != nil {
-// 		t.Fatalf("reconcile: (%v)", err)
-// 	}
-// 	if !res.Requeue {
-// 		t.Error("reconcile did not requeue")
-// 	}
-
-// 	dep := &v1beta1.Deployment{}
-// 	err = cl.Get(context.TODO(), req.NamespacedName, dep)
-
-// 	if err != nil {
-// 		// FAILS HERE
-// 		t.Fatalf("get deployment: (%v)", err)
-// 	}
-
-// 	dSize := *dep.Spec.Replicas
-// 	if dSize != size {
-// 		t.Errorf("dep size (%d) is not the expected size (%d)", dSize, size)
-// 	}
-// }
 
 func TestReconcileMobileSecurityService_update(t *testing.T) {
 	type fields struct {
@@ -110,8 +30,9 @@ func TestReconcileMobileSecurityService_update(t *testing.T) {
 		wantErr bool
 	}{
 		{
+			name: "should requeue",
 			fields: fields{
-				instance: &v1alpha1.MobileSecurityService{
+				instance: &mobilesecurityservicev1alpha1.MobileSecurityService{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "mobile-security-service-app",
 						Namespace: "mobile-security-service-operator",
@@ -131,7 +52,7 @@ func TestReconcileMobileSecurityService_update(t *testing.T) {
 
 			objs := []runtime.Object{tt.fields.instance}
 
-			tt.fields.scheme.AddKnownTypes(v1alpha1.SchemeGroupVersion, tt.fields.instance)
+			tt.fields.scheme.AddKnownTypes(mobilesecurityservicev1alpha1.SchemeGroupVersion, tt.fields.instance)
 
 			cl := fake.NewFakeClient(objs...)
 
@@ -153,6 +74,104 @@ func TestReconcileMobileSecurityService_update(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ReconcileMobileSecurityService.update() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReconcileMobileSecurityService_create(t *testing.T) {
+	type fields struct {
+		scheme *runtime.Scheme
+	}
+	type args struct {
+		instance *mobilesecurityservicev1alpha1.MobileSecurityService
+		kind     string
+		err      error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    reconcile.Result
+		wantErr bool
+	}{
+		{
+			name: "should return an error",
+			fields: fields{
+				scheme: scheme.Scheme,
+			},
+			args: args{
+				instance: &mobilesecurityservicev1alpha1.MobileSecurityService{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "mobile-security-service-app",
+						Namespace: "mobile-security-service-operator",
+					},
+					Spec: v1alpha1.MobileSecurityServiceSpec{
+						Size:          1,
+						MemoryLimit:   "512Mi",
+						MemoryRequest: "512Mi",
+					},
+				},
+				kind: DEEPLOYMENT,
+				err:  errors.NewInternalError(errs.New("Internal Server Error")),
+			},
+			want:    reconcile.Result{},
+			wantErr: true,
+		},
+		{
+			name: "should create and return a new deployment",
+			fields: fields{
+				scheme: scheme.Scheme,
+			},
+			args: args{
+				instance: &mobilesecurityservicev1alpha1.MobileSecurityService{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "mobile-security-service-app",
+						Namespace: "mobile-security-service-operator",
+					},
+					Spec: v1alpha1.MobileSecurityServiceSpec{
+						Size:          1,
+						MemoryLimit:   "512Mi",
+						MemoryRequest: "512Mi",
+					},
+				},
+				kind: DEEPLOYMENT,
+				err:  errors.NewNotFound(schema.GroupResource{Group: "api/v1", Resource: "ResourceName"}, "Not Found"),
+			},
+			want:    reconcile.Result{Requeue: true},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			objs := []runtime.Object{tt.args.instance}
+
+			tt.fields.scheme.AddKnownTypes(mobilesecurityservicev1alpha1.SchemeGroupVersion, tt.args.instance)
+
+			cl := fake.NewFakeClient(objs...)
+
+			r := &ReconcileMobileSecurityService{
+				client: cl,
+				scheme: tt.fields.scheme,
+			}
+
+			req := reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      tt.args.instance.Name,
+					Namespace: tt.args.instance.Namespace,
+				},
+			}
+
+			reqLogger := log.WithValues("Request.Namespace", req.Namespace, "Request.Name", req.Name)
+
+			got, err := r.create(tt.args.instance, reqLogger, tt.args.kind, tt.args.err)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReconcileMobileSecurityService.create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReconcileMobileSecurityService.create() = %v, want %v", got, tt.want)
 			}
 		})
 	}
