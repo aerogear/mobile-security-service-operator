08c96774cda93388be1f18149bc7fe0dc1f8eecb
Camila Macedo <camilamacedo86@gmail.com>
2019-05-14 15:16:47 +0100

    feat(test) : unit test

---
+++ pkg/controller/mobilesecurityservice/controller_test.go
@@ -0,0 +1,84 @@
+package mobilesecurityservice
+
+import (
+	"context"
+
+	"github.com/aerogear/mobile-security-service-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	mobilesecurityservicev1alpha1 "github.com/aerogear/mobile-security-service-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	"github.com/aerogear/mobile-security-service-operator/pkg/utils"
+	routev1 "github.com/openshift/api/route/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"testing"
+)
+
+var (
+	name            = "mobile-security-service"
+	namespace       = "mobile-security-service-operator"
+	replicas  int32 = 1
+
+	instance = &mobilesecurityservicev1alpha1.MobileSecurityService{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.MobileSecurityServiceSpec{
+			Size:                    1,
+			MemoryLimit:             "512Mi",
+			MemoryRequest:           "512Mi",
+			ClusterProtocol:         "http",
+			ConfigMapName:           "mss-config",
+			RouteName:               "mss-route",
+			SkipNamespaceValidation: true,
+		},
+	}
+)
+
+func TestReconcileMobileSecurityService_ReconcileController(t *testing.T) {
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+
+	//Add route Openshift scheme
+	if err := routev1.AddToScheme(s); err != nil {
+		t.Fatalf("Unable to add route scheme: (%v)", err)
+	}
+
+	s.AddKnownTypes(routev1.SchemeGroupVersion, &routev1.Route{})
+	s.AddKnownTypes(mobilesecurityservicev1alpha1.SchemeGroupVersion, instance)
+	objs := []runtime.Object{instance}
+
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	// Create a ReconcileMemcached object with the scheme and fake client.
+	r := &ReconcileMobileSecurityService{client: cl, scheme: s}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	// Check the result of reconciliation to make sure it has the desired state.
+	if !res.Requeue {
+		t.Error("reconcile did not requeue request as expected")
+	}
+
+	route := &routev1.Route{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: utils.GetRouteName(instance), Namespace: instance.Namespace}, route)
+	if err != nil {
+		t.Fatalf("get route: (%v)", err)
+	}
+
+}
