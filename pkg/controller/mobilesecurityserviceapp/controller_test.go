dbc8f29f9298ea3e81d1bcabe840c3683bff5dbd
Damien Murphy <damurphy@redhat.com>
2019-05-16 15:08:58 +0100

    tests(controller): implements reconcile() test cases

--- pkg/controller/mobilesecurityserviceapp/controller_test.go
+++ pkg/controller/mobilesecurityserviceapp/controller_test.go
@@ -2,15 +2,26 @@ package mobilesecurityserviceapp
 
 import (
 	errs "errors"
+	//"context"
 	"reflect"
 	"testing"
 
 	mobilesecurityservicev1alpha1 "github.com/aerogear/mobile-security-service-operator/pkg/apis/mobilesecurityservice/v1alpha1"
+	"github.com/aerogear/mobile-security-service-operator/pkg/utils"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	routev1 "github.com/openshift/api/route/v1"
+
 	corev1 "k8s.io/api/core/v1"
+	// "k8s.io/api/extensions/v1beta1"
+
 	"k8s.io/apimachinery/pkg/api/errors"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -28,7 +39,45 @@ var (
 			AppName: "test-app",
 			AppId:   "test-app-id",
 		},
+	}
 
+	instanceTwo = mobilesecurityservicev1alpha1.MobileSecurityServiceApp{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mobile-security-service",
+			Namespace: "mobile-security-service-2",
+		},
+		Spec: mobilesecurityservicev1alpha1.MobileSecurityServiceAppSpec{
+			AppName: "test-app",
+			AppId:   "test-app-id",
+		},
+	}
+
+	mssInstance = mobilesecurityservicev1alpha1.MobileSecurityService{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mobile-security-service-app",
+			Namespace: "mobile-security-service-operator",
+		},
+		Spec: mobilesecurityservicev1alpha1.MobileSecurityServiceSpec{
+			Size:                    1,
+			MemoryLimit:             "512Mi",
+			MemoryRequest:           "512Mi",
+			ClusterProtocol:         "http",
+			ConfigMapName:           "mss-config",
+			RouteName:               "mss-route",
+			SkipNamespaceValidation: true,
+		},
+	}
+
+	route = routev1.Route{
+		TypeMeta: v1.TypeMeta{
+			APIVersion: "route.openshift.io/v1",
+			Kind:       "Route",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      utils.GetRouteName(&mssInstance),
+			Namespace: mssInstance.Namespace,
+			Labels:    getAppLabels(mssInstance.Name),
+		},
 	}
 )
 
@@ -198,36 +247,104 @@ func TestReconcileMobileSecurityServiceApp_buildFactory(t *testing.T) {
 }
 
 func TestReconcileMobileSecurityServiceApp_Reconcile(t *testing.T) {
-	type fields struct {
-		client client.Client
-		scheme *runtime.Scheme
+	// objects to track in the fake client
+	objs := []runtime.Object{
+		&instance,
+		&instanceTwo,
+		&mssInstance,
+		&route,
 	}
-	type args struct {
-		request reconcile.Request
+
+	r := getReconciler(objs)
+
+	// mock request to simulate Reconcile() being called on an event for a watched resource
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		},
 	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    reconcile.Result
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+
+	// request goes through loop once 
+	//res, err := r.Reconcile(req)
+	_, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := &ReconcileMobileSecurityServiceApp{
-				client: tt.fields.client,
-				scheme: tt.fields.scheme,
-			}
-			got, err := r.Reconcile(tt.args.request)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("ReconcileMobileSecurityServiceApp.Reconcile() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ReconcileMobileSecurityServiceApp.Reconcile() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+
+
+
+
+
+	// // create configMap & ensure reconciliation is requeued
+	// configMap := &corev1.ConfigMap{}
+	// err = r.client.Get(context.TODO(), req.NamespacedName, configMap)
+	// if !res.Requeue {
+	// 	t.Error("reconcile did not requeue request as expected")
+	// }
+
+	// // create Deployment & ensure reconciliation is requeued
+	// res, err = r.Reconcile(req)
+	// if err != nil {
+	// 	t.Fatalf("reconcile: (%v)", err)
+	// }
+	// if !res.Requeue {
+	// 	t.Error("reconcile did not requeue request as expected")
+	// }
+
+	// // check if the deployment has been created
+	// dep := &v1beta1.Deployment{}
+	// err = r.client.Get(context.TODO(), req.NamespacedName, dep)
+	// if err != nil {
+	// 	t.Fatalf("get deployment: (%v)", err)
+	// }
+
+	// res, err = r.Reconcile(req)
+	// if err != nil {
+	// 	t.Fatalf("reconcile: (%v)", err)
+	// }
+
+	// if !res.Requeue {
+	// 	t.Error("reconcile did not requeue request as expected")
+	// }
+
+	// // check if the service has been created
+	// service := &corev1.Service{}
+	// err = r.client.Get(context.TODO(), req.NamespacedName, service)
+	// if err != nil {
+	// 	t.Fatalf("get service: (%v)", service)
+	// }
+
+	// res, err = r.Reconcile(req)
+	// if err != nil {
+	// 	t.Fatalf("reconcile: (%v)", err)
+	// }
+
+	// if res.Requeue {
+	// 	t.Error("reconcile unexpectedly requeued request")
+	// }
+
+	// route := &routev1.Route{}
+	// err = r.client.Get(context.TODO(), types.NamespacedName{Name: utils.GetRouteName(&mssInstance), Namespace: instance.Namespace}, route)
+	// if err != nil {
+	// 	t.Fatalf("get route: (%v)", err)
+	// }
+}
+
+func getReconciler(objs []runtime.Object) *ReconcileMobileSecurityServiceApp {
+	s := scheme.Scheme
+
+	routev1.AddToScheme(s)
+
+	s.AddKnownTypes(mobilesecurityservicev1alpha1.SchemeGroupVersion, &mobilesecurityservicev1alpha1.MobileSecurityServiceApp{})
+
+	s.AddKnownTypes(mobilesecurityservicev1alpha1.SchemeGroupVersion, &mobilesecurityservicev1alpha1.MobileSecurityService{})
+	s.AddKnownTypes(routev1.SchemeGroupVersion, &routev1.Route{})
+	s.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.ConfigMap{}, &corev1.Service{})
+
+	// create a fake client to mock API calls
+	cl := fake.NewFakeClient(objs...)
+	// create a ReconcileMobileSecurityService object with the scheme and fake client
+	return &ReconcileMobileSecurityServiceApp{client: cl, scheme: s}
 }
